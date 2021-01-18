5803cd6f62df94dce0f11b488ae69639b4002326
qiubingyu77 <qiubingyu77@163.com>
2021-01-18 20:01:02 +0800

    118

--- basic-infa-rest/src/main/java/org/basic/infa/rest/domain/Result.java
+++ basic-infa-rest/src/main/java/org/basic/infa/rest/domain/Result.java
@@ -1,13 +1,16 @@
 package org.basic.infa.rest.domain;
 
+import lombok.Data;
+
 /**
  * @autor qiubingyu
  * @date 2021/1/18
  * @name GlobalResult
  **/
-public class Result<T> {
+@Data
+public class Result {
     int code;
     String msg;
-    T t;
+    Object data;
 
 }
